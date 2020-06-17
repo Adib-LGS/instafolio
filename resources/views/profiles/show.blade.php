becbb112f49dbf636d024c79a67c526b8ec9132a
Adib-Legastelois <adib.legastelois@icloud.com>
2020-06-17 01:12:22 +0100

    First commit + Basic UI Design

---
+++ resources/views/profiles/show.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+            <div class="container">
+                <div class="row mt-4">
+                    <div class="col-4">
+                        <img src="https://otakuusamagazine.com/wp-content/uploads/2018/08/nl-jet-black-cowboy-bebop.jpg" class="rounded-circle" width="150px" height="150px">
+                    </div>
+                    <div class="col-8">
+                        <div class="d-flex">
+                            <div class="h4 mr-3 pt-2">Adib Legastelois</div>
+                            <button class="btn btn-primary btn-sm ">S'abonner</button>
+                        </div>
+                        <div class="d-flex mt-3">
+                            <div class="mr-3"><strong>0</strong> publications</div>
+                            <div class="mr-3"><strong>0</strong> abonnés</div>
+                            <div class="mr-3"><strong>0</strong> abonnement</div>
+                        </div>
+                        <div class="mt-3">
+                            <div>Welcome to my Portfolio</div>
+                            <div>Below you can see my projects</div>
+                            <a href="https://github.com/Adib-LGS">My GitHub Profile</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-4">
+                        <img src="https://avatars3.githubusercontent.com/u/58850596?s=460&v=4" class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://avatars3.githubusercontent.com/u/58850596?s=460&v=4" class="w-100">
+                    </div>
+                    <div class="col-4">
+                        <img src="https://avatars3.githubusercontent.com/u/58850596?s=460&v=4" class="w-100">
+                    </div>
+                </div>
+            </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/**Auhtentification systeme */
+Auth::routes();
+
+/**DashBoard */
+Route::get('/home', 'HomeController@index')->name('home');
+
+/**RestFull Pathern for the Controllers */
+Route::get('/profiles/{username}', 'ProfileController@show')->name('profiles.show');
