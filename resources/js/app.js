9d8dc6ad83512dd424ace3b21a0e205ef5b74dc4
Adib-Legastelois <adib.legastelois@icloud.com>
2020-07-15 17:55:51 +0100

    FInal Heroku

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,7 +20,6 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('follow-button', require('./components/FollowButton.vue').default);
-Vue.component('searchTask', require('./components/TaskComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
